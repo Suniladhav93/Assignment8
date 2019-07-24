9ca1baef4d76dc57a8436fd770703c6a41bc33d7
unknown <suniladhav10@gmail.com>
2019-07-24 11:33:46 +0530

    First commit

---
+++ src/com/model/Assignment8.java
@@ -0,0 +1,35 @@
+package com.model;
+
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeSuite;
+
+public class Assignment8 
+{
+	public WebDriver driver;
+  @Test
+  public void test1() 
+  {
+	  //click on my account
+	  driver.findElement(By.xpath("//html/body/div/div/div[3]/div/div[4]/ul/li[1]/a")).click();
+	  //login
+	  driver.findElement(By.cssSelector("input[id='email']")).sendKeys("suniladhav10@gmail.com");
+	  driver.findElement(By.cssSelector("input[id='pass']")).sendKeys("p@ssword1");
+	  driver.findElement(By.xpath("//button[@id='send2']")).click();
+  }
+  @BeforeSuite
+  public void beforeSuite()
+  {
+	  System.setProperty("webdriver.chrome.driver", "E:\\Selenium\\chromedriver.exe");
+	  driver =new ChromeDriver();
+	  driver.get("http://live.guru99.com");
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().pageLoadTimeout(15, TimeUnit.SECONDS);
+  }
+
+}
